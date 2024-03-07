2f645d68645a165c1e416dfb1324d13ca583c633
alessandrog6498 <alessandrog6498@gmail.com>
2024-03-07 10:39:00 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let prova = document.getElementById("divisione1");
+let test = {
+	"prova": "ciao",
+	"test": "come mi chiamo",
+	"lista": [1, 2, 3, 4, false],
+	"boleanoo": true
+}
+prova.innerHTML = test;
+
